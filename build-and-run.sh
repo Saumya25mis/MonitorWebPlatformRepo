47e5bb58e65cbe6ab9938c42ccefa50f6e4cda5d
Saumya Mishra <saumya@Saumyas-MacBook-Pro.local>
2023-11-03 19:05:54 +0530

    changes

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,7 +1,4 @@
-#!/bin/sh
 
-# Build the target image using Dockerfile2
 docker build -t my-custom-image -f /DockerFile2 .
 
-# Run a container from the built image
 docker run -t my-custom-image:latest
